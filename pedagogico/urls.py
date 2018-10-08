e87718576206492f5f222da8f0f52ea50905558c
wedgomes <40505932+wedgomes@users.noreply.github.com>
2018-10-08 15:42:36 -0300

    Espero que não se apague tudo

--- pedagogico/urls.py
+++ pedagogico/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('criar_turma', views.criar_turma, name="criar_turma"),
     path('cadastrar_turmas', views.cadastrar_turmas, name="cadastrar_turmas"),
     path('cadastrar_aula', views.cadastrar_aula, name='cadastrar_aula'),
+    path('criar_aula', views.cadastrar_aulas, name='cadastrar_aulas'),
     path('frequencia', views.frequencia, name='frequencia'),
     path('triagem_pedagogica', views.triagem_pedagogica, name='triagem_pedagogica'),
 ]
--- pedagogico/views.py
+++ pedagogico/views.py
@@ -1,27 +1,36 @@
 from django.shortcuts import render
 from .models import *
+
+
 # Create your views here.
 
 def index(request):
-	return render(request, 'pedagogico/index.html', {})
+    return render(request, 'pedagogico/index.html', {})
+
 
 def criar_turma(request):
-	professores = Funcionario.objects.all()
-	return render(request, 'pedagogico/criar_turma.html', {"professores":professores})
+    professores = Funcionario.objects.all()
+    return render(request, 'pedagogico/criar_turma.html', {"professores": professores})
+
 
 def cadastrar_aula(request):
     turmas = Turma.objects.all()
     aulas = Aula.objects.all()
-    return render(request, 'pedagogico/cadastrar_aula.html', {"turmas":turmas, "aulas":aulas})
+    usuarioTurma = UsuarioTurma.objects.all()
+    return render(request, 'pedagogico/cadastrar_aula.html',
+                  {"turmas": turmas, "aulas": aulas, "usuarioTurma": usuarioTurma})
+
 
 def frequencia(request):
     turmas = Turma.objects.all()
     usuarios = Usuario.objects.all()
     aulas = Aula.objects.all()
-    return render(request, 'pedagogico/frequencia.html', {"usuarios":usuarios, "turmas":turmas, "aulas":aulas})
+    return render(request, 'pedagogico/frequencia.html', {"usuarios": usuarios, "turmas": turmas, "aulas": aulas})
+
 
 def triagem_pedagogica(request):
     return render(request, 'pedagogico/triagem_pedagogica.html')
+
 
 def cadastrar_turmas(request):
     professor = Funcionario.objects.get(nome=request.POST["professor"])
@@ -30,7 +39,8 @@ def cadastrar_turmas(request):
         turno = False
     else:
         turno = True
-    turma = Turma(nome=request.POST['nome'],turno= turno);
+
+    turma = Turma(nome=request.POST['nome'], turno=turno);
     turma.save()
     protuma = ProfessorTurma(professor=professor, turma=turma)
     protuma.save()
@@ -39,4 +49,15 @@ def cadastrar_turmas(request):
     print("veioaqui")
     print(request.POST)
 
+def cadastrar_aulas(resquest):
+    turma = Turma.objects.get(nomeTurma=resquest.POST["turma"])
+    turma.save()
+    data = Aula.objects.get(data=resquest.POST["data"])
+    data.save()
+    conteudo = Aula.objects.get(conteudo=resquest.POST["conteudo"])
+    conteudo.save()
 
+    aulaSalva = Aula(data=data, nomeTurma=turma, conteudo=conteudo)
+    aulaSalva.save()
+
+    return render(resquest, 'pedagogico/index.html')
