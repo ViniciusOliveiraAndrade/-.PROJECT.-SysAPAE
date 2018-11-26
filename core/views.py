f30eda9a0b91de1dc20a644c8f3f71a6aa7ffac3
ViniciusOliveiraAndrade <viniciusibsm@gmail.com>
2018-11-25 21:49:54 -0300

    Adição da função de cadastrar usuario, alteração do menu de perfil, criação do layout base para o core

--- core/views.py
+++ core/views.py
@@ -16,22 +16,25 @@ def index(request):
 
     return render(request,'core/index.html', dados)
 
+@login_required
 def registrarFuncionario(request):
     if request.method == "POST":
-    	pass
-    else:
     	
-    	return render(request,'core/registrar.html',{})
+        user = User.objects.create_user(request.POST['nomeuser'], request.POST['email'], '123456Mm')
+        pnome, unome = getNomeFuncionario(request.POST['nome'])
+        user.first_name = pnome
+        user.last_name = unome
+        user.save()
+        cargo = Cargo.objects.get(nome=request.POST['cargo'])
 
-def cadastrar_funcionario(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid:
-            form.save()
-            return redirect('core:index')
+        Funcionario.objects.create(user=user, cargo=cargo)
+        return redirect('core:index')
+        
+         
     else:
-        form = UserCreationForm()
-        return render(request,'core/registrar.html',{"form":form})    
+    	cargos = Cargo.objects.all()
+    	return render(request,'core/registrar.html',{'cargos':cargos})
+ 
 
 def login(request):
     #ja verifica se tem dados no banco de dados e se não cria dados base
@@ -58,17 +61,19 @@ def login(request):
             return render(request,'core/login.html')
         else:
             return redirect('core:index')
-
+@login_required
 def logout(request):
     logout_f(request)
     return render(request,'core/logout.html')
-    
+
+@login_required   
 def registro(request):
     acessos = Registro_acesso.objects.all().order_by('-data_acesso')
     acoes = Registro_acao.objects.all().order_by('-data_acao')
     dados = {"acessos":acessos, "acoes":acoes}
     return render(request,'core/registro.html',dados)
 
+@login_required
 def help(request):
     
     dados = {}
